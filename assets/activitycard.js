5e10e12b83dc460e93419ca0d73bce39a59bc834
Rajat Jha <rcjha23@gmail.com>
2025-09-22 22:05:59 +0545

    clid

--- assets/activitycard.js
+++ assets/activitycard.js
@@ -492,5 +492,9 @@ function Notif(iconClass, message, time = 5000) {
 document.addEventListener("DOMContentLoaded", () => {
   if (window.location.hash === "#contact-alert") {  
     Notif("fa fa-envelope", "Your message was sent!", 10000);
-  }
+  } else if (window.location.href.includes("?fromonlyfans")) {
+    Notif("fa-solid fa-face-rolling-eyes", "Onlyfans? Check THIS out instead!", 10000);
+  } else if (window.location.href.includes("?ig")) {
+    Notif("fa-solid fa-ghost", "From instagram? Drop me a text if you liked this!", 10000);
+  } 
 });
