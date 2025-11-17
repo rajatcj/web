cde799da943e301a3c7e430fc45baad9d9bfde11
Rajat Jha <rcjha23@gmail.com>
2025-11-18 01:16:19 +0545

    PWA

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+    console.log('Service worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('Service worker activated');
+});
